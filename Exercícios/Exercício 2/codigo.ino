78b150fcaad01fd660dbe205649e039faf478f1a
Zovitinho <99494600+Zovitinho@users.noreply.github.com>
2022-03-25 17:35:14 -0300

    Update codigo.ino

--- Exercícios/Exercício 2/codigo.ino
+++ Exercícios/Exercício 2/codigo.ino
@@ -1,1 +1,19 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+}
 
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
